16e81004d78b759e5272d4548f9e938646d9f606
Geice Sousa Pinho <geicesousa@outlook.com>
2023-03-03 00:10:04 -0300

    finalização do readme

--- src/quiz.js
+++ src/quiz.js
@@ -2,21 +2,42 @@ import  { calculatePercentage } from "./data.js";
 
 const formQuiz = document.querySelector("#quiz");
 
-formQuiz.addEventListener("click", (e)=>{
+formQuiz.addEventListener("click", (e) => {
   const elemento = e.target;
   const ehRadio = elemento.type === "radio";
   const ehCorreta = ehRadio && elemento.value === "correta";
 
-  if(ehRadio){
+  if (ehRadio) {
     elemento.parentNode.parentNode.dataset.score = ehCorreta;
     //fazer selecionar apenas uma vez
+
+    // elemento.checked === true;
+    // console.log(elemento.closest("p input").disabled = "disabled") FAZ PARAR DE FUNCIONAR O INPUT JÁ SELECIONADO
+    const divQuiz = document.querySelector(".divQuiz");
+    console.log(divQuiz); //com All não funciona
+
+    divQuiz.classList.add("desabilita");
+
+    // elemento.disabled = "disabled"
   }
-})
+
+  // closest("p input") vai pegar o ancestral mais proximo no caso o p do input
+});
+
+const enviaQuiz = document.querySelector("#btn");
+enviaQuiz.addEventListener("click", calculaAcertos);
+
+function calculaAcertos (){
+  const divQuiz = document.querySelectorAll(".divQuiz");
+  const mensage = document.querySelector(".mensage");
+  const jogarNovamente = document.querySelector(".jogarNovamente");
+  const inputCorretas = document.querySelectorAll("input[type=radio][value=correta]");
+  const qtdDeCorretas = inputCorretas.checked;
+  const percentage = calculatePercentage(qtdDeCorretas, divQuiz.length);
 
 
-// export const calculatePercentage = (dataList, dataListFiltered)=> {    
-//   return parseInt((dataListFiltered*100)/dataList)
-// }
-    
-// const percentage = calculatePercentage(characters.length, filteredSearch.length);
-//   percentageReturn.innerHTML = `Essa lista contém ${filteredSearch.length} personagens, que equivale á ${percentage}% dos personagens totais`; 
+  mensage.innerHTML = `Parabéns! Você acertou ${percentage} das perguntas.`
+  jogarNovamente.style.display = "block";
+  console.log("parabens! funcionou")
+  //jogarNovamente.classList.remove("jogarNovamente")
+}
